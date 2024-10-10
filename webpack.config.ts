a8e1689d0235670ee810037c501fc5047af41bd1
yjgaia <yjdev88@gmail.com>
2024-10-10 16:46:56 +0900

    Add webpack configuration and update package.json

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  entry: {
+    "bundle": "./src/client.ts",
+    __less: "./public/style/main.less",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: "css-loader",
+          options: {
+            url: false,
+          },
+        }, "less-loader"],
+      },
+      {
+        test: /\.ya?ml$/,
+        use: "yaml-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".less"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("public"),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+};
+
+export default config;
